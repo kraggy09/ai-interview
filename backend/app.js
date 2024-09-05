149e58520ccec7b0a10cd11d27e6f44f51ba8c9d
Kaif Shaikh <kaifshaikh2013.sk@gmail.com>
2024-09-05 15:35:30 +0530

    Working on the cors error

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,32 @@ import dotenv from "dotenv";
 import router from "./routes/routes.js";
 import cors from "cors";
 import connection from "./db/dbConfig.js";
+import cookieParser from "cookie-parser"; // Import cookie-parser
 
 const app = express();
 dotenv.config({
   path: path.resolve("backend", "config", "config.env"),
 });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-intervue.vercel.app",
+];
 
 app.use(
   cors({
-    origin: true, // Allow any origin
-    credentials: true, // Allow credentials (cookies)
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
